46d2d0881de67da1cb23ff383b52a7563863ec0c
saito-dat <kuroutoFOV90@gmail.com>
2022-06-30 13:30:14 +0900

    Initial commit

---
+++ v0630/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v0630
+{
+    public partial class Form1 : Form
+    {
+        int vx = -5;
+        int vy = -10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+            if (label1.Left < 0)
+                vx = 5;
+            if (label1.Top < 0)
+                vy = 10;
+            if (label1.Right > ClientSize.Width)
+                vx = -5;
+            if (label1.Bottom > ClientSize.Height)
+                vy = -10;
+            Point spos = MousePosition;
+            Point fpos = PointToClient(spos);
+            label2.Left = fpos.X - label2.Width / 2;
+            label2.Top = fpos.Y - label2.Height / 2;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
